5d259e8d6fd50f6a1ec2f9f6366b246d23e1ac90
brandon <brandon@gmail.com>
2022-03-27 19:48:03 +0700

    mod

--- cg.sh
+++ cg.sh
@@ -5,12 +5,12 @@ git pull origin fat
 git submodule init
 git submodule update --remote
 
-PROJECT="member2"
+PROJECT="member"
 GitReversion=`git rev-parse HEAD`
 BuildTime=`date +'%Y.%m.%d.%H%M%S'`
 BuildGoVersion=`go version`
 
 go build -ldflags "-X main.gitReversion=${GitReversion}  -X 'main.buildTime=${BuildTime}' -X 'main.buildGoVersion=${BuildGoVersion}'" -o $PROJECT
-# bigbet88
-sshpass -p $1 scp $PROJECT root@192.168.80.194:/home/centos/workspace/agame/$PROJECT
+# cg
+sshpass -p $1 scp $PROJECT testp3@34.92.240.177:/home/centos/cg/$PROJECT
 

8cabe06ec7a2f5cbd0d60a7e957f853e310a53a4
bran <bran@git.com>
2022-04-18 04:46:05 -0700

    mod build.sh

--- build.sh
+++ build.sh
@@ -11,9 +11,8 @@ BuildTime=`date +'%Y.%m.%d.%H%M%S'`
 BuildGoVersion=`go version`
 
 go build -ldflags "-X main.gitReversion=${GitReversion}  -X 'main.buildTime=${BuildTime}' -X 'main.buildGoVersion=${BuildGoVersion}'" -o $PROJECT
-mv $PROJECT /opt/data/deploy/va/$PROJECT
-# shellcheck disable=SC2164
-cd /opt/data/deploy/va/$PROJECT
+mv $PROJECT /opt/deploy/cg/$PROJECT
+cd /opt/deploy/cg/$PROJECT
 
 git pull
 git commit -am "${GitReversion}"

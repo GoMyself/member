22cee80612337fda491737776e982d8ef4b999a9
bran <bran@git.com>
2022-05-30 09:37:20 -0700

    mod

--- prod.sh
+++ prod.sh
@@ -5,17 +5,17 @@ git pull origin main
 git submodule init
 git submodule update
 
+DIR=$(pwd)
 PROJECT="member"
 GitReversion=`git rev-parse HEAD`
 BuildTime=`date +'%Y.%m.%d.%H%M%S'`
 BuildGoVersion=`go version`
 
 go build -ldflags "-X main.gitReversion=${GitReversion}  -X 'main.buildTime=${BuildTime}' -X 'main.buildGoVersion=${BuildGoVersion}'" -o $PROJECT
-mv $PROJECT /opt/deploy/cg/$PROJECT
 # shellcheck disable=SC2164
 cd /opt/deploy/cg/$PROJECT
-
 git pull
+mv $DIR/$PROJECT /opt/deploy/cg/$PROJECT
 git commit -am "${GitReversion}"
 git push
 

6a06dca62d7b90b7a2227c5052aeac41b517872f
raiden <cg19raiden@gmail.com>
2022-07-09 18:09:28 +0800

    新手教程

--- model/tutorial.go
+++ model/tutorial.go
@@ -13,7 +13,7 @@ func TutorialRead(uid string) error {
 		Score:  float64(1),
 		Member: uid,
 	}
-	err := meta.MerchantRedis.ZAdd(ctx, key, &z).Err()
+	err := meta.MerchantRedis.SAdd(ctx, key, &z).Err()
 	if err != nil {
 		return pushLog(err, helper.RedisErr)
 	} else {
@@ -24,17 +24,12 @@ func TutorialRead(uid string) error {
 func TutorialState(uid string) (bool, error) {
 
 	key := fmt.Sprintf(`%s:tutorial:%s`, meta.Prefix, uid)
-	z := redis.Z{
-		Member: uid,
-	}
-	bools, err := meta.MerchantRedis.SMIsMember(ctx, key, &z).Result()
-	if err != nil {
-		return false, pushLog(err, helper.RedisErr)
+	bools := meta.MerchantRedis.SMIsMember(ctx, key, uid).Val()
+
+	if len(bools) > 0 {
+		return bools[0], nil
 	} else {
-		if len(bools) > 0 {
-			return bools[0], nil
-		} else {
-			return false, nil
-		}
+		return false, nil
 	}
+
 }
